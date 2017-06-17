0fd7fb5a0b5f7f9e9fb6c1fb923733278ba44017
Aditya Pewekar <Aditya Pewekar>
2017-06-18 00:37:53 +0530

    SPA Angular MVC Demo Self Practice

---
+++ SPAAngularJSMVC/Scripts/Controller/CommentController.js
@@ -0,0 +1,6 @@
+﻿app.controller('CommentController', function ($scope, $location, $window) {
+    $scope.Name = "Aditya";
+    $scope.Company = "GeBBS";
+    $scope.Role = "Software Engineer";
+});
+
